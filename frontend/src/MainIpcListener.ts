8a756e3e0031a55f524b2a9c4805e3ff9dd97b9f
lvfeng <lvfeng.org@gmail.com>
2026-05-22 23:29:42 +0800

    fix(任务前端): 移除调试日志

--- frontend/src/MainIpcListener.ts
+++ frontend/src/MainIpcListener.ts
@@ -21,7 +21,6 @@ export function iniListener() {
   })
 
   Events.On('taskStatus-updateTask', (event: any) => {
-    console.log('[MainIpc] taskStatus-updateTask received:', JSON.stringify(event.data))
     const taskList = event.data as TaskProgressDTO[]
     useTaskStore().updateTask(taskList)
   })
